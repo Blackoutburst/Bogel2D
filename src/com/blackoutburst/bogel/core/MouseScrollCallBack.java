cad7382d1d9aebb935d4280f8acb3f109cd362f4
Blackoutburst <blackoutburst@hotmail.com>
2021-11-25 16:05:00 +0100

    Added Mouse callback doc

--- src/com/blackoutburst/bogel/core/MouseScrollCallBack.java
+++ src/com/blackoutburst/bogel/core/MouseScrollCallBack.java
@@ -2,12 +2,20 @@ package com.blackoutburst.bogel.core;
 
 import org.lwjgl.glfw.GLFWScrollCallbackI;
 
+/**
+ * <h1>Mouse Scroll CallBack</h1>
+ * 
+ * <p>
+ * Update mouse scroll <b>do not call</b>
+ * </p>
+ * 
+ * @since 0.1
+ * @author Blackoutburst
+ */
 public class MouseScrollCallBack implements GLFWScrollCallbackI {
 
 	@Override
 	public void invoke(long window, double xoffset, double yoffset) {
 		Mouse.setScroll((float) (Mouse.getScroll() + yoffset));
 	}
-
-
 }
