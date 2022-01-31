86890818e89bea6195f8d01daca216277d2f0803
Blackoutburst <blackoutburst@hotmail.com>
2022-02-01 00:14:19 +0100

    Fixed texture edge artifact

--- src/com/blackoutburst/bogel/graphics/RenderManager.java
+++ src/com/blackoutburst/bogel/graphics/RenderManager.java
@@ -7,6 +7,7 @@ import com.sun.istack.internal.NotNull;
 import org.lwjgl.opengl.GL11;
 
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.GL_CLAMP_TO_BORDER;
 
 /**
  * <h1>RenderManager</h1>
@@ -186,6 +187,9 @@ public class RenderManager {
 	 * @author Blackoutburst
 	 */
 	protected static void setTextureParameter(@NotNull Shape shape) {
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
 		if (shape.texture.missing) {
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
